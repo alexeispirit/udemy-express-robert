2b92bbedc99429575b4124471b1b3fa394c8c0f5
alexeispirit <alexeispirit@gmail.com>
2019-10-15 14:58:50 +0500

    16-Putting-on-helmet-on-express-and-other-express-middleware

---
+++ 05-express-201/helmetAndOthers.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const app = express();
+
+app.use(helmet());
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/ajax", (req, res) => {
+  console.log(req.body);
+  res.send("TEST");
+});
+
+app.listen(3000);
