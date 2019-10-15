10c4b1c7fb372d055a6c4d870cd876e1c5eb2f38
alexeispirit <alexeispirit@gmail.com>
2019-10-15 15:22:04 +0500

    17-Responding-with-JSON

--- 05-express-201/helmetAndOthers.js
+++ 05-express-201/helmetAndOthers.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.post("/ajax", (req, res) => {
   console.log(req.body);
-  res.send("TEST");
+  res.json(["test", 1, 2, 3]);
 });
 
 app.listen(3000);
