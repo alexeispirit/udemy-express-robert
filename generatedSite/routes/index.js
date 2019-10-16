9ad275fb2f396c6e9ae442ea00147cd5507fbff1
alexeispirit <alexeispirit@gmail.com>
2019-10-16 11:52:19 +0500

    32-Headers

--- generatedSite/routes/index.js
+++ generatedSite/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  const date = new Date(1969, 6, 20);
+  res.set("Date", date);
+  res.set("Cache-Control", "no-store");
+  res.set("Content-Type", "text/html");
+
+  // fresh and stale
+  // fresh returns true if it's not stale
+  console.log(req.fresh);
+  console.log(req.stale);
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
