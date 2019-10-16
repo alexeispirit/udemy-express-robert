b80eb6744a67418dadbbca65e2c9436e4e0c14d3
alexeispirit <alexeispirit@gmail.com>
2019-10-16 16:59:05 +0500

    45-Search-routes-with-router.use

--- movieApi/routes/search.js
+++ movieApi/routes/search.js
@@ -1,9 +1,40 @@
 var express = require("express");
+
+const movies = require("../data/movies");
+const people = require("../data/people");
+
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+function queryRequired(req, res, next) {
+  const searchTerm = req.query.query;
+  if (!searchTerm) {
+    res.json({ msg: "Query is required" });
+  } else {
+    next();
+  }
+}
+
+router.use(queryRequired);
+
+// GET /search/movie
+router.get("/movie", function(req, res, next) {
+  const searchTerm = req.query.query;
+  const results = movies.filter(movie => {
+    let found =
+      movie.overview.includes(searchTerm) || movie.title.includes(searchTerm);
+    return found;
+  });
+  res.json({ results });
+});
+
+// GET /search/person
+router.get("/person", function(req, res, next) {
+  const searchTerm = req.query.query;
+  const results = people.filter(person => {
+    let found = person.name.includes(searchTerm);
+    return found;
+  });
+  res.json({ results });
 });
 
 module.exports = router;
