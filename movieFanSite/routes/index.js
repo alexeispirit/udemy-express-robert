7b49ca3d53de6cb3b26a40d764407c3c30204401
alexeispirit <alexeispirit@gmail.com>
2019-10-16 15:22:09 +0500

    40-MovieApi-project-overview-and-setup

--- movieFanSite/routes/index.js
+++ movieFanSite/routes/index.js
@@ -3,8 +3,10 @@ const request = require("request");
 
 var router = express.Router();
 
-const apiKey = "1fb720b97cc13e580c2c35e1138f90f8";
-const apiBaseUrl = "http://api.themoviedb.org/3";
+// const apiKey = "1fb720b97cc13e580c2c35e1138f90f8";
+const apiKey = "123456789";
+// const apiBaseUrl = "http://api.themoviedb.org/3";
+const apiBaseUrl = "http://localhost:3030";
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
 const imageBaseUrl = "http://image.tmdb.org/t/p/w300";
 
